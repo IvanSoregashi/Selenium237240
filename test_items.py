99d7d51df8a59ac25350436acc4eeb01a847b606
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-03-21 11:32:51 +0500

    fixed the locator

--- test_items.py
+++ test_items.py
@@ -2,6 +2,9 @@ from selenium.webdriver.common.by import By
 
 def test1(browser):
     browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
-    assert browser.find_element(By.XPATH, "//button[@type='submit']").is_displayed()
-    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_enabled()
+    button = browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket")
+    assert button.is_displayed()
+    assert button.is_enabled()
+    #print(button.text)
+    #print(button.get_attribute("data-loading-text"))
 
