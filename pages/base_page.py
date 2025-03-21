89adce3b14cdc20996912d56ea2719736dd772ba
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-03-21 16:02:45 +0500

    base_page implementation

---
+++ pages/base_page.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.chrome.webdriver import WebDriver
+
+class BasePage:
+    URL = ""
+
+    def __init__(self, driver: WebDriver):
+        self.driver = driver
+
+    def open(self):
+        self.driver.get(self.URL)
