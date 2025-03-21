39f4f5c8007087c2d13e6c988f6255113becc6a8
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-03-21 11:22:18 +0500

    button testing test

--- test_items.py
+++ test_items.py
@@ -1,6 +1,7 @@
-import time
-
+from selenium.webdriver.common.by import By
 
 def test1(browser):
-    browser.get("http://selenium1py.pythonanywhere.com/")
-    time.sleep(3)
+    browser.get("http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/")
+    assert browser.find_element(By.XPATH, "//button[@type='submit']").is_displayed()
+    assert browser.find_element(By.CSS_SELECTOR, "button.btn-add-to-basket").is_enabled()
+
